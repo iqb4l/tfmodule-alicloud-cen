934c724fd116b679e69037bbe02535200cd84576
Iqb4l <iqbalwahid51@gmail.com>
2025-10-25 21:48:35 +0700

    Initial commit: cen module

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,9 @@
 output "cen_id" {
   description = "ID dari CEN Instance yang dibuat."
   value       = alicloud_cen_instance.cen.id
+}
 
+output "tr_vpc_attachment_id" {
+  value       = alicloud_cen_transit_router_vpc_attachment.this.transit_router_attachment_id
+  description = "The id of attachment between TR and VPC."
 }
